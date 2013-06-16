94288ab7a5e618672fec0b1b65895a51a608ab98
Chandresh Pant <cp@cpant.in>
2013-06-16 21:31:12 +0530

    first_feature

---
+++ features/step_definitions/website_steps.rb
@@ -0,0 +1,7 @@
+When(/^I visit the homepage$/) do
+  visit root_path
+end
+
+Then(/^it should work$/) do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/website.feature
@@ -0,0 +1,6 @@
+Feature: As a visitor
+  I should be able to visit the homepage
+
+  Scenario: visit homepage
+    When I visit the homepage
+    Then it should work
